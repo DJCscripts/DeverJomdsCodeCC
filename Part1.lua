969ada2b8675bd2503bc87df3ff781cccfcb38ce
DJCscripts <banckrot202@gmail.com>
2026-09-13 08:58:22 +0300

    Update Part1.lua

--- Part1.lua
+++ Part1.lua
@@ -1,6 +1,6 @@
 --[[
-	ULTIMATE MENU v8.3 - LOADER
-	Author: DJCscript | TG: @DeverJomdsCodeCC
-	Key: DeverJomdsCodeCC
+    ULTIMATE MENU v8.3 - LOADER
+    Author: DJCscript | TG: @DeverJomdsCodeCC
+    Key: DeverJomdsCodeCC
 ]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DJCscript/DeverJomdsCodeCC/main/Part1.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DJCscript/DeverJomdsCodeCC/main/Part1..."))
