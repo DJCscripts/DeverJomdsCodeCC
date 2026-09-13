097c539002d4530088d3c88faf67fea73eed9697
DJCscripts <banckrot202@gmail.com>
2026-09-13 07:24:27 +0300

    Create UltimateMenu.lua

---
+++ UltimateMenu.lua
@@ -0,0 +1,7 @@
+--[[
+	ULTIMATE MENU v6.2 | ESP + AimBot + Silent Aim + Freecam + FOV
+	Author: DJC | Telegram: @DeverJomdsCodeCC
+	Universal Key: DeverJomdsCodeCC
+	WARNING: Use at your own risk! Not verified by ScriptBlox.
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DJC/DeverJomdsCodeCC/main/UltimateMenu.lua", true))()
